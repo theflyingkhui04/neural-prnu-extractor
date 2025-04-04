71ef29ffe196c22feac06cfde987a22e80adc9b2
Can Duc Khoi <133144430+theflyingkhui04@users.noreply.github.com>
2025-04-05 04:47:38 +0700

    Remove outdated license information and comments from main module

--- ffdnet/__main__.py
+++ ffdnet/__main__.py
@@ -1,22 +1,3 @@
-""" __main__.py
-Main module that parses command line arguments.
-
-Copyright (C) 2018, Matias Tassano <matias.tassano@parisdescartes.fr>
-
-This program is free software: you can use, modify and/or
-redistribute it under the terms of the GNU General Public
-License as published by the Free Software Foundation, either
-version 3 of the License, or (at your option) any later
-version. You should have received a copy of this license along
-this program. If not, see <http://www.gnu.org/licenses/>.
-
-Later authors:
-
-- Simone Alghisi (simone.alghisi-1@studenti.unitn.it)
-- Samuele Bortolotti (samuele.bortolotti@studenti.unitn.it)
-- Massimo Rizzoli (massimo.rizzoli@studenti.unitn.it)
-"""
-
 import argparse
 import ffdnet.processors as processors
 
@@ -43,6 +24,8 @@ def get_args():
   if 'func' not in parsed_args:
     parser.print_usage()
     parser.exit(1)
+
+  return parsed_args
 
 def main(args):
   r"""Main function."""
